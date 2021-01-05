55380314fdde966715815599666d894bfc415b4d
Tomasz Gadek <gadektomek@gmail.com>
2021-01-05 22:20:12 +0100

    init project

---
+++ src/main/java/com/demo/springboot/rest/DocumentApiController.java
@@ -0,0 +1,13 @@
+package com.demo.springboot.rest;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+public class DocumentApiController {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(DocumentApiController.class);
+
+}
